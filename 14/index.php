2f97a8ce592a338481dfd0b547a38e2adb8f2362
kristinagulanich <gulyanich.k.v@gmail.com>
2021-11-16 11:17:40 +0200

    выполнила задание 14

--- 14/index.php
+++ 14/index.php
@@ -9,13 +9,13 @@ class MenuItem{
 
 $sandwich = new MenuItem();
 
-$sandwich ->name = 'Pork';
+$sandwich ->name = 'Pork-Sandwich';
 $sandwich ->price = 138;
 $sandwich ->size = '435 gm';
 
 $salad = new MenuItem();
 
-$salad ->name = 'Veggie';
+$salad ->name = 'Veggie Salad';
 $salad ->price = 98;
 $salad ->size = '380 gm';
 
@@ -25,4 +25,10 @@ $drink ->name = 'Tea';
 $drink ->price = 33;
 $drink ->size = '300 ml';
 
-echo 'MenuItem :' . $sandwich ->name . 'Price : ' . $sandwich ->price . 'Size : ' . $salad ->size . "\n";
+echo str_repeat("-", 16) . "\n";
+echo 'MenuItem: ' . $sandwich ->name . "\n" . 'Price: ' . $sandwich ->price ."\n" . 'Size: ' . $sandwich ->size . "\n";
+echo str_repeat("-", 16) . "\n";
+echo 'MenuItem: ' . $salad ->name . "\n" . 'Price: ' . $salad ->price ."\n" . 'Size: ' . $salad ->size . "\n";
+echo str_repeat("-", 16) . "\n";
+echo 'MenuItem: ' . $drink ->name . "\n" . 'Price: ' . $drink ->price ."\n" . 'Size: ' . $drink ->size . "\n";
+echo str_repeat("-", 16) . "\n";
